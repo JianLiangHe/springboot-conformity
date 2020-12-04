54b539b0bf7119f28e7c20ca089b9b56c639b643
hejianliang <844131681@qq.com>
2020-12-04 09:30:27 +0800

    activemq消息消费者

---
+++ springboot-conformity-activemq-consumer/src/main/java/edu/conformity/SpringbootConformityActivemqConsumer.java
@@ -0,0 +1,13 @@
+package edu.conformity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConformityActivemqConsumer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConformityActivemqConsumer.class, args);
+	}
+	
+}
