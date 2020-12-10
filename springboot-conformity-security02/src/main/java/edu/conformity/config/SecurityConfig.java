3f70b5f4d63829ad67d10f3a434c0d567ef4a867
hejianliang <844131681@qq.com>
2020-12-10 17:25:41 +0800

    整合spring security，02案例

---
+++ springboot-conformity-security02/src/main/java/edu/conformity/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package edu.conformity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * spring security配置类
+ * @author hejianliang
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * 登录处理
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/v1/index").hasRole("admin")
+			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated();
+		http.csrf().disable();
+	}
+
+	/**
+	 * 忽略拦截
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 设置拦截忽略url，会直接过滤该url，不会经过spring security过滤器链拦截
+		web.ignoring().antMatchers("/v1/index/test");
+		// 设置拦截忽略文件夹，可以对静态资源放行
+		web.ignoring().antMatchers("/css/**", "/js/**");
+	}
+	
+}
