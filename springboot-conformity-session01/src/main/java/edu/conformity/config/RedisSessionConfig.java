3863516fe4dd3d34268d1a7913f5fc2d9110705b
hejianliang <844131681@qq.com>
2020-12-17 11:18:49 +0800

    增加Spring Session配置

--- springboot-conformity-session01/src/main/java/edu/conformity/config/RedisSessionConfig.java
+++ springboot-conformity-session01/src/main/java/edu/conformity/config/RedisSessionConfig.java
@@ -1,11 +1,16 @@
 package edu.conformity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 // maxInactiveIntervalInSeconds 默认是1800秒过期，这里改为60秒
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
+// RedisFlushMode有两个参数：ON_SAVE（表示在response commit前刷新缓存），IMMEDIATE（表示只要有更新，就刷新缓存）
+// redisNamespace : redis中的key
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60, 
+	redisFlushMode = RedisFlushMode.ON_SAVE, 
+	redisNamespace = "springboot-conformity-session01")
 public class RedisSessionConfig {
 
 }
