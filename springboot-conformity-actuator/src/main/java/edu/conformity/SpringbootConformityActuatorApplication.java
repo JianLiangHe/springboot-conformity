747812469342096df089cb6348861ed4b09c607e
hejianliang <844131681@qq.com>
2021-01-20 15:45:46 +0800

    集成actuator

---
+++ springboot-conformity-actuator/src/main/java/edu/conformity/SpringbootConformityActuatorApplication.java
@@ -0,0 +1,13 @@
+package edu.conformity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConformityActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConformityActuatorApplication.class, args);
+	}
+	
+}
