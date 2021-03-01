38f03f67fd273444b54648e743c60bde5a309ab5
hejianliang <844131681@qq.com>
2021-03-01 09:15:51 +0800

    添加测试案例

--- springboot-conformity-lambda/src/test/java/edu/conformity/test/LambdaTest.java
+++ springboot-conformity-lambda/src/test/java/edu/conformity/test/LambdaTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import edu.conformity.lambdainter.NoReturnMultiParam;
 import edu.conformity.lambdainter.NoReturnNoParam;
 import edu.conformity.lambdainter.NoReturnOneParam;
+import edu.conformity.lambdainter.ReturnMultiParam;
 import edu.conformity.lambdainter.ReturnNoParam;
 import edu.conformity.lambdainter.ReturnOneParam;
 
@@ -40,7 +41,7 @@ public class LambdaTest {
 		noReturnMultiParam.method(2, 3);
 	}
 	
-	@Test
+	//@Test
 	public void returnNoParam() {
 		ReturnNoParam returnNoParam = () -> {
 			System.out.println("returnNoParam");
@@ -50,9 +51,24 @@ public class LambdaTest {
 		System.out.println("result:" + 8);
 	}
 	
-	@Test
+	//@Test
 	public void returnOneParam() {
-		 
+		ReturnOneParam returnOneParam = (int a) -> {
+			System.out.println("returnOneParam param:" + a);
+			return a + 1;
+		};
+		int result = returnOneParam.method(5);
+		System.out.println("result:" + result);
+	}
+	
+	@Test
+	public void returnMultiParam() {
+		ReturnMultiParam returnMultiParam = (int a, int b) -> {
+			System.out.println("returnMultiParam a:" + a + ", b:" + b);
+			return a + b;
+		};
+		int result = returnMultiParam.method(5, 5);
+		System.out.println("result: " + result);
 	}
 	
 }
