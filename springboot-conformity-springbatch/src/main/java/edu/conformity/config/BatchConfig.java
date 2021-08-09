d6001aadfd1ee1a7034eccffb6f42a034743ea92
hejianliang <844131681@qq.com>
2021-08-09 16:53:03 +0800

    完善配置类

--- springboot-conformity-springbatch/src/main/java/edu/conformity/config/BatchConfig.java
+++ springboot-conformity-springbatch/src/main/java/edu/conformity/config/BatchConfig.java
@@ -1,7 +1,86 @@
 package edu.conformity.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import edu.conformity.service.impl.ItemProcessorService;
+import edu.conformity.service.impl.ItemProcessorService2;
+import edu.conformity.service.impl.ItemReaderService;
+import edu.conformity.service.impl.ItemReaderService2;
+import edu.conformity.service.impl.ItemWriterService;
+import edu.conformity.service.impl.JobListener;
+
 public class BatchConfig {
 
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
 	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	private ItemReaderService itemReaderService;
+
+	@Autowired
+	private ItemReaderService2 itemReaderService2;
+	
+	@Autowired
+	private ItemProcessorService itemProcessorService;
+	
+	@Autowired
+	private ItemProcessorService2 itemProcessorService2;
+	
+	@Autowired
+	private ItemWriterService itemWriterService;
+	
+	@Autowired
+	private JobListener jobListener;
+	
+	public Job singleStepJob() {
+		return jobBuilderFactory.get("singleStepJob")
+				.incrementer(new RunIdIncrementer())
+				.listener(listener())
+                .start(uppercaseStep())
+                .build();
+	}
+	
+	@Bean
+    public Job multiBoundStepsJob() {
+        return jobBuilderFactory.get("multiBoundStepsJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener())
+                .start(uppercaseStep())
+                .next(addMessageStep())
+                .build();
+    }
+
+    @Bean
+    public Step uppercaseStep() {
+        return stepBuilderFactory.get("uppercaseStep")
+                .<String, String>chunk(1)
+                .reader(itemReaderService)
+                .processor(itemProcessorService)
+                .writer(itemWriterService).build();
+    }
+
+    @Bean
+    public Step addMessageStep() {
+        return stepBuilderFactory.get("addMessageStep")
+                .<String, String>chunk(1)
+                .reader(itemReaderService2)
+                .processor(itemProcessorService2)
+                .writer(itemWriterService).build();
+    }
+
+    @Bean
+    public JobExecutionListener listener() {
+        return jobListener;
+    }
 	
 }
