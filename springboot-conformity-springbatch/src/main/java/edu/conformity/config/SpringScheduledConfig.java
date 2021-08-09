98ff0a2a41af700aa1e993ac709fcf717417b035
hejianliang <844131681@qq.com>
2021-08-09 17:30:03 +0800

    增加SpringScheduledConfig 配置类

---
+++ springboot-conformity-springbatch/src/main/java/edu/conformity/config/SpringScheduledConfig.java
@@ -0,0 +1,28 @@
+package edu.conformity.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringScheduledConfig {
+
+	@Autowired
+	private Job singleStepJob;
+	
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Scheduled(cron = "0/5 * * * * ?")
+	public void demoScheduled() throws Exception {
+		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+										.toJobParameters();
+		
+		jobLauncher.run(singleStepJob, jobParameters);
+	}
+	
+}
